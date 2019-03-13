b19a4d2e64226cde344fc5106a953b460e20042c
Lucas Sanchez <lucas.sanchez@epitech.eu>
2019-03-13 18:11:25 +0100

    [Lucas] Add of sdl

--- lib/SDL2/include/SDL2.hpp
+++ lib/SDL2/include/SDL2.hpp
@@ -10,20 +10,19 @@
 
 #include <SDL2/SDL.h>
 #include "IDisplayModule.hpp"
+#include <iostream>
+#include <chrono>
 
 class DisplayModule : public IDisplayModule {
-    private:
-        std::string _name = "SDL2";
-
 	public:
-		DisplayModule();
-		virtual ~DisplayModule();
+		DisplayModule() = default;
+		virtual ~DisplayModule() = default;
 
         void init();
         void stop();
         const std::string &getName() const { return _name; };
 
-        bool isOpen();
+        bool isOpen() { return _isOpen; };
         void clear();
         void display();
 
@@ -32,6 +31,72 @@ class DisplayModule : public IDisplayModule {
         
         float getTime();
         std::map<Input, bool> catchInput();
+
+    private:
+        std::string _name = "SDL2";
+        std::chrono::steady_clock::time_point _clock;
+        bool _isOpen = false;
+        SDL_Window *_win = nullptr;
+        SDL_Renderer *_render = nullptr;
 };
+
+const int RGBA[8][3] = {
+    {0, 0, 0},         //BLACK
+    {230, 25, 75},     //RED
+    {60, 180, 75},     //GREEN
+    {255, 225, 25},    //YELLOW
+    {0, 130, 200},     //BLUE
+    {240, 50, 230},    //MAGENTA
+    {70, 240, 240},    //CYAN
+    {255, 255, 255},   //WHITE
+};
+
+const std::map<Input, SDL_Keycode> SDL_Key = {
+    {LEFT_ARROW_KEY,    SDLK_LEFT},
+    {RIGHT_ARROW_KEY,   SDLK_RIGHT},
+    {UP_ARROW_KEY,      SDLK_UP},
+    {DOWN_ARROW_KEY,    SDLK_DOWN},
+    {SPACE_KEY,         SDLK_SPACE},
+    {RETURN_KEY,        SDLK_RETURN},
+    {ERASE_KEY,         SDLK_BACKSPACE},
+    {ESCAPE_KEY,        SDLK_ESCAPE},
+    {A_KEY,             SDLK_a},
+    {B_KEY,             SDLK_b},
+    {C_KEY,             SDLK_c},
+    {D_KEY,             SDLK_d},
+    {E_KEY,             SDLK_e},
+    {F_KEY,             SDLK_f},
+    {G_KEY,             SDLK_g},
+    {H_KEY,             SDLK_h},
+    {I_KEY,             SDLK_i},
+    {J_KEY,             SDLK_j},
+    {K_KEY,             SDLK_k},
+    {L_KEY,             SDLK_l},
+    {M_KEY,             SDLK_m},
+    {N_KEY,             SDLK_n},
+    {O_KEY,             SDLK_o},
+    {P_KEY,             SDLK_p},
+    {Q_KEY,             SDLK_q},
+    {R_KEY,             SDLK_r},
+    {S_KEY,             SDLK_s},
+    {T_KEY,             SDLK_t},
+    {U_KEY,             SDLK_u},
+    {V_KEY,             SDLK_v},
+    {W_KEY,             SDLK_w},
+    {X_KEY,             SDLK_x},
+    {Y_KEY,             SDLK_y},
+    {Z_KEY,             SDLK_z},
+    {ONE_KEY,           SDLK_1},
+    {TWO_KEY,           SDLK_2},
+    {THREE_KEY,         SDLK_3},
+    {FOUR_KEY,          SDLK_4},
+    {FIVE_KEY,          SDLK_5},
+    {SIX_KEY,           SDLK_6},
+    {SEVEN_KEY,         SDLK_7},
+    {EIGHT_KEY,         SDLK_8},
+    {NINE_KEY,          SDLK_9},
+    {ZERO_KEY,          SDLK_0}
+};
+
 
 #endif /* !SDL2_HPP_ */
--- lib/SDL2/src/SDL2.cpp
+++ lib/SDL2/src/SDL2.cpp
@@ -5,46 +5,103 @@
 ** SFML.cpp
 */
 
-#include "SFML.hpp"
-
-DisplayModule::DisplayModule()
-{
-}
-
-DisplayModule::~DisplayModule()
-{
-}
+#include "SDL2.hpp"
 
 void DisplayModule::init()
 {
+    auto start = std::chrono::steady_clock::now();
+
+    _clock = start;
+    _isOpen = true;
+    SDL_Init(SDL_INIT_VIDEO);
+    _win = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        1632, 880, SDL_WINDOW_SHOWN);
+    _render = SDL_CreateRenderer(_win, -1, SDL_RENDERER_ACCELERATED);
 }
 
 void DisplayModule::stop()
 {
-}
-
-bool DisplayModule::isOpen()
-{
-    return true;
+    _isOpen = false;
+    SDL_DestroyWindow(_win);
+    SDL_Quit();
 }
 
 void DisplayModule::display()
 {
+    if (!_isOpen)
+        return;
+    SDL_RenderPresent(_render);
 }
 
 void DisplayModule::clear()
 {
+    if (!_isOpen)
+        return;
+    SDL_SetRenderDrawColor(_render, 0, 0, 0, 255);
+    SDL_RenderClear(_render);
 }
 
-void DisplayModule::drawText(int x, int y, const std::string &name) const
+void DisplayModule::drawText(int x, int y, const std::string &str) const
 {
+    (void)x;
+    (void)y;
+    (void)str;
+    if (!_isOpen)
+        return;
 }
 
-void DisplayModule::drawShape(int x, int y, std::vector<std::vector<Color> > pixels) const
+void DisplayModule::drawShape(int x, int y, std::vector<std::vector<Color> > pixels)
 {
+    SDL_Rect rec;
+    int tmp = x;
+
+    if (!_isOpen)
+        return;
+    for (auto &i: pixels) {
+        for (auto &j: i) {
+            if (j == -1) {
+                x += 16;
+                continue;
+            }
+            rec.x = x;
+            rec.y = y;
+            rec.h = 16;
+            rec.w = 16;
+            SDL_SetRenderDrawColor(_render, RGBA[j][0], RGBA[j][1], RGBA[j][2], 255);
+            SDL_RenderFillRect(_render, &rec);
+            x += 16;
+        }
+        x = tmp;
+        y += 16;
+    }
+}
+
+float DisplayModule::getTime()
+{
+    auto end = std::chrono::steady_clock::now();
+    auto start = _clock;
+
+    if (!_isOpen)
+        return 0;
+    return double(double(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()) / 1000000);
 }
 
 std::map<Input, bool> DisplayModule::catchInput()
 {
-    return {};
+    SDL_Event event;
+    std::map<Input, bool> map;
+
+    if (!_isOpen)
+        return map;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT) {
+        this->stop();
+        return map;
+    } else if (event.type == SDL_KEYDOWN)
+        return map;
+    for (auto &i: SDL_Key) {
+        if (event.key.keysym.sym == i.second)
+            map[i.first] = true;
+    }
+    return map;
 }
