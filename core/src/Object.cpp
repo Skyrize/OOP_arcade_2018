ae101fccf39ca7a3ae179233962bc03aaa26ce88
Lucas <lucas.sanchez@epitech.eu>
2019-03-31 19:42:56 +0200

    Another correction for mouliinette

---
+++ core/src/Object.cpp
@@ -0,0 +1,82 @@
+/*
+** EPITECH PROJECT, 2018
+** OOP_arcade_2018
+** File description:
+** Object.cpp
+*/
+
+#include "Object.hpp"
+#include <iostream>
+
+Object::Object(const std::string &name, SpriteSheet &spriteSheet, const std::pair<float, float> &position)
+: name(name), sprite(*this, spriteSheet), movement(*this)
+{
+	this->movement.setPosition(position.first, position.second);
+}
+
+Object::Object(const std::string &name, Sprite &sprite, const std::pair<float, float> &position)
+: name(name), sprite(*this, sprite), movement(*this)
+{
+	this->movement.setPosition(position.first, position.second);
+}
+
+Object::~Object()
+{
+};
+
+void Object::display(IDisplayModule *display)
+{
+	const std::pair<float, float> &position = this->movement.getPosition();
+
+	display->drawShape(GET_X(position), GET_Y(position), sprite.getSprite());
+}
+
+float Object::update(IDisplayModule *display, std::map<std::string, Object *> &objects)
+{
+
+    float actualTime = display->getTime();
+    if (actualTime < oldTime || oldTime == 0)
+        oldTime = actualTime;
+    float delta = actualTime - oldTime;
+    
+	movement.move(delta, objects);
+    sprite.animate(delta);
+    oldTime = actualTime;
+	return delta;
+}
+
+void Object::manageEvents(std::map<Input, bool> &inputs)
+{
+	(void)inputs;
+}
+
+void Object::eventHit(Object *other)
+{
+	(void)other;
+}
+
+void Object::eventReachedDestination()
+{
+}
+
+Object::MovementComponent &Object::getMovement()
+{
+	return this->movement;
+}
+
+Object::AnimationComponent &Object::getAnimation()
+{
+	return sprite;
+}
+
+const std::string &Object::getName() const
+{
+	return this->name;
+}
+
+/*					MovementComponent					*/
+
+// static float lerp(float a, float b, float fraction)
+// {
+// 	return (a * (1.0 - fraction) + b * fraction);
+// }
